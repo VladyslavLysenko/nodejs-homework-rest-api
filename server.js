8b9fa11c40910fd393bad242863ee9371a23f113
VladyslavLysenko <Dodog12362@gmail.com>
2023-02-26 22:24:46 +0200

    ok

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
-const app = require('./app')
+// AlvEjFY5oTnLAM35
+const app = require("./app");
+const mongoose = require("mongoose");
+const DB_HOST =
+  "mongodb+srv://Vlad:AlvEjFY5oTnLAM35@cluster0.fx2z7ky.mongodb.net/contacts_reader?retryWrites=true&w=majority";
+mongoose.set("strictQuery", true);
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000);
+    console.log("Database connection successful");
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000")
+// })
+
