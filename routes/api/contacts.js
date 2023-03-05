18888ff36106a052e77764acb7000a8256703796
VladyslavLysenko <Dodog12362@gmail.com>
2023-03-05 15:33:30 +0200

    updated

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -18,40 +18,4 @@ router.patch("/:id/favorite", isValidId, validateBody(schemas.updateFavoriteSche
 
 router.delete("/:id", isValidId, ctrl.removeContact);
 
-
-
-
-
-
-
-// router.patch("/:contactId/favorite", isValidId, async (req, res, next) => {
-//   try {
-//     const { error } = schemas.updateFavoriteSchema.validate(req.body);
-//     if (error) {
-//       throw HttpError(400, error.message);
-//     }
-//     const { contactId } = req.params;
-//     const result = await ctrl.updateFavorite(contactId, req.body);
-//     if (!result) {
-//       throw HttpError(404, "Not found");
-//     }
-//     res.json(result);
-//   } catch (error) {
-//     next(error);
-//   }
-// });
-
-// router.delete("/:contactId", isValidId, async (req, res, next) => {
-//   try {
-//     const { contactId } = req.params;
-//     const result = await ctrl.removeContact(contactId);
-//     if (!result) {
-//       throw HttpError(404, "Not found");
-//     }
-//     res.status(200).json({ message: "contact deleted" });
-//   } catch (error) {
-//     next(error);
-//   }
-// });
-
 module.exports = router;
