4cee0882a342de341ed4de9b5062dae3d8668953
VladyslavLysenko <Dodog12362@gmail.com>
2023-03-12 13:15:25 +0200

    fix

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -8,7 +8,8 @@ const listContacts = async (req, res) => {
   const { _id: owner } = req.user;
   const { page = 1, limit = 10 } = req.query;
   const skip = (page - 1) * limit;
-  const result = await Contact.find({ owner }, {skip, limit}).populate("owner", "name email");;
+  const result = await Contact.find({ owner }, { skip, limit }).populate("owner", "name email");
+  console.log(result);
   res.json(result);
 };
 
